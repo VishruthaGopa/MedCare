2294a98449a0b38a4038a0cbbf9d6275effb5ca3
AlexanderL <al.x.300000@gmail.com>
2021-11-13 19:10:53 -0500

    commit

--- app.py
+++ app.py
@@ -5,24 +5,59 @@ from flask import render_template
 import jyserver.Flask as jsf
 import numpy as np
 import pandas as pd 
+import re
+import requests
+from bs4 import BeautifulSoup
+
 
 app = Flask(__name__)  
 
 @jsf.use(app)
 class App:
     def __init__(self):
-        self.input = ""
-    
+        self.symptom = ""
+        
     
     def test(self):
+        output = []
+        self.symptom = str(self.js.document.getElementById("sym").value)
        
         syd=pd.read_csv('diffsydiw.csv').dropna()
         dia=pd.read_csv('dia_t.csv').dropna()
         sym=pd.read_csv('sym_t.csv').dropna()
         
-        self.js.document.getElementById('count').innerHTML = 5
+        diseases = sym[sym['symptom'].str.contains(self.symptom, flags=re.IGNORECASE)]
+        if not diseases.empty:
+            
+            sympID = diseases["syd"].iloc[0]
+            test = syd[syd['syd']==sympID].sort_values('wei',ascending=False)['did']
+            for i in range(4):
+                output.append(dia[dia['did']==test.iloc[i]]['diagnose'].iloc[0])
+            
+            self.js.document.getElementById('message').innerHTML = "Here are some common diseases that have \"" +diseases["symptom"].iloc[0] + "\" as a symptom."
+            self.js.document.getElementById('one').innerHTML =" 1." + output[0]
+            self.js.document.getElementById('two').innerHTML = "2." + output[1]
+            self.js.document.getElementById('three').innerHTML = "3." + output[2]
+            self.js.document.getElementById('four').innerHTML = "4." + output[3]
+            self.js.document.getElementById('five').innerHTML = "5." + output[4]
 
 
+            for item in output:
+                goog_search = "https://www.google.com/search?q="+item+"&rlz=1C1RXQR_enCA929CA929&oq=asdasdasd&aqs=chrome..69i57j0i10l3j46i10i175i199j0i10l5.3408j0j9&sourceid=chrome&ie=UTF-8"
+                r = requests.get(goog_search)
+                soup = BeautifulSoup(r.text, "html.parser")
+                print(soup.find('cite').text)
+
+        else:
+            self.js.document.getElementById('message').innerHTML = "Sorry, we could not find any diseases with that symptom. Please retry or check your spelling!"
+            self.js.document.getElementById('one').innerHTML = ""
+            self.js.document.getElementById('two').innerHTML = ""
+            self.js.document.getElementById('three').innerHTML = ""
+            self.js.document.getElementById('four').innerHTML = ""
+            self.js.document.getElementById('five').innerHTML = ""
+   
+
+        
 
 @app.route("/")
 def index():
@@ -37,7 +72,7 @@ def about():
     """
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
 
 
--- script.py
+++ script.py
@@ -1,26 +1,41 @@
-import numpy as np
-import pandas as pd 
+# import numpy as np
+# import pandas as pd 
+
+import requests
+from bs4 import BeautifulSoup
+
+item = "cough"
+html = requests.get('https://www.google.com/search?q=ice cream').text
+soup = BeautifulSoup(html, 'html.parser')
+
+# locating .tF2Cxc class
+# calling for <a> tag and then calling for 'href' attribute
+link = soup.select('.yuRUbf a')['href']
+print(link)
 
 
+# syd=pd.read_csv('diffsydiw.csv').dropna()
+# dia=pd.read_csv('dia_t.csv').dropna()
+# sym=pd.read_csv('sym_t.csv').dropna()
 
+# # print(sym.head())
+# # print(dia.head())
 
-syd=pd.read_csv('diffsydiw.csv').dropna()
-dia=pd.read_csv('dia_t.csv').dropna()
-sym=pd.read_csv('sym_t.csv').dropna()
+# inp = 4
 
-print(sym.head())
-print(dia.head())
+# test = syd[syd['syd']==2].sort_values('wei',ascending=False)['did']
+# print(test.iloc[0])
+# for i in range(5):
+#     print(dia[dia['did']==test.iloc[i]])
 
-print(syd[syd['syd']==2].sort_values('wei',ascending=False).head())
-print(dia[dia['did']==56])
+# symp = "Lowe"
+# print(sym[sym['symptom'].str.contains(symp)]) 
+# # print(syd[syd['did']==22])
+# # print(sym[sym['syd']==128])
+# # print(sym[sym['syd']==264])
 
-
-print(syd[syd['did']==56])
-print(sym[sym['syd']==128])
-print(sym[sym['syd']==264])
-
-print(syd[syd['syd']==128].sort_values('wei',ascending=False).head())
-print(dia[dia['did']==54])
-print(dia[dia['did']==159])
-print(dia[dia['did']==177])
-print(dia[dia['did']==192])
+# # print(syd[syd['syd']==128].sort_values('wei',ascending=False).head())
+# # print(dia[dia['did']==54])
+# # print(dia[dia['did']==159])
+# # print(dia[dia['did']==177])
+# # print(dia[dia['did']==192])
